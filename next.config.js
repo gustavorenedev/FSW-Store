257395506a58abe0e8371e1c637fac17c1d407f3
Gustavo René <renegustavo42@gmail.com>
2023-10-22 02:32:55 -0300

    chore: add s3 domain to next config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['https://www.google.com/']
-    },
-}
+  images: {
+    domains: [
+      "fsw-store.s3.sa-east-1.amazonaws.com",
+      "https://www.google.com/",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
